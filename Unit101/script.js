60e386ce3258bec26639784ca97af22495b071a9
anphung1708 <anphung@Ans-MacBook-Pro.local>
2015-02-24 15:23:00 -0500

    Unit101

--- Unit101/script.js
+++ Unit101/script.js
@@ -4,5 +4,5 @@ var hoursPerDay = 24;
 var daysPerWeek = 7;
 var weeksPerYear = 52;
 var answer =prompt("How old are you?");
-answer= secondsPerMin*minsPerHour*hoursPerDay*daysPerWeek*weeksPerYear;
+answer= answer*secondsPerMin*minsPerHour*hoursPerDay*daysPerWeek*weeksPerYear;
 document.write("You have been alive "+answer+" seconds in the earth!");
